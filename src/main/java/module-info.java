3a54ad70b96c615b464c4950fb81eedcbc9204d6
Nicolas Torres <nicolastorres55@gmail.com>
2024-04-02 02:09:26 +0200

    feat: much improved styling and layouting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.ndts.optalgj {
 	requires javafx.controls;
 	requires javafx.fxml;
 
-	requires org.kordamp.bootstrapfx.core;
 	requires org.controlsfx.controls;
 
 	opens org.ndts.optalgj to javafx.fxml;
