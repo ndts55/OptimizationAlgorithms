98b18b5e1a8dda7c6476ac69dadfbb4aba48ec29
Nicolas Torres <nicolastorres55@gmail.com>
2024-04-28 01:41:12 +0200

    chore: node instead of wrapper

--- src/main/java/org/ndts/optalgj/algs/TraversalStrategy.java
+++ src/main/java/org/ndts/optalgj/algs/TraversalStrategy.java
@@ -3,10 +3,10 @@ package org.ndts.optalgj.algs;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public abstract class TraversalStrategy<Solution, Wrapper extends GreedyNode<Solution>> {
+public abstract class TraversalStrategy<Solution, Node extends GreedyNode<Solution>> {
 	private final AtomicBoolean cancelled = new AtomicBoolean(false);
 
-	protected abstract List<Wrapper> traverse(Wrapper wrapper);
+	protected abstract List<Node> traverse(Node node);
 
 	protected void cancel() {
 		cancelled.setRelease(true);
